bad3b1c9587a04c0cd6a97b33d2a79fc3fd84943
redwone <redwoneali13@gmail.com>
2025-11-09 23:29:51 +0530

    codeforce problem

---
+++ matrix.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arry[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arry[i][j]);
+        }
+    }
+    int sum1 = 0, sum2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                sum1 += arry[i][j];
+            }
+            else if (i + j == n-1)
+            {
+                sum2 += arry[i][j];
+            }
+        }
+    }
+    int x = 0;
+    x = sum1 - sum2;
+    printf("%d", x);
+}
